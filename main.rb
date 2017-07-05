75c842da5ed517269f4afca1a5af12daae318dc9
rabadan731 <rabadan731@yandex.ru>
2017-07-05 16:08:07 +0300

    fix readme

---
+++ main.rb
@@ -0,0 +1,11 @@
+# Программа магический шар
+# версия 0.1
+require_relative "text"
+
+# выводим приветствие
+puts welcome_texts.sample
+
+# Делаем случайный вывод с случайной задержкой
+sleep(rand(5))
+puts
+puts answers.sample
